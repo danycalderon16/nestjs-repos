ad6de873c53e862cdb1dbbdcc9a5fc90ed980276
Daniel Calderon <danycalvir@gmail.com>
2023-12-11 23:29:25 -0700

    finish crud

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -1,43 +1,105 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { validate as isUUID } from 'uuid';
 import { Product } from './entities/product.entity';
-
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 @Injectable()
 export class ProductsService {
+  private readonly logger = new Logger('ProductsService');
 
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
-  ){}
+  ) {}
 
   async create(createProductDto: CreateProductDto) {
     try {
       const product = this.productRepository.create(createProductDto);
       await this.productRepository.save(product);
 
-      return product
+      return product;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Error al crear el producto');      
+      this.handleDBExeption(error);
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto:PaginationDto) {
+    try {
+      const {limit=10,offset=0}=paginationDto;
+      return await this.productRepository.find({
+        take:limit,
+        skip:offset,
+      });
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException(
+        'Unexpected error, check server logs',
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(term: string) {
+    let product:Product;
+
+    if(isUUID(term)){
+      product=await this.productRepository.findOneBy({id:term})    
+    }else{
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where(`UPPER(title) =:title or slug =:slug`,{
+          title:term.toUpperCase(),
+          slug:term.toLowerCase(),
+        }).getOne();
+    }
+    if (!product)
+      throw new NotFoundException(`Product with ${term} does not exits`);
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id:id,
+      ...updateProductDto
+    })
+
+    if(!product) throw new NotFoundException(`Product with ${id} does not exist`);
+    
+    try {
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBExeption(error)
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    try {
+      const res = await this.productRepository.delete(id);
+      if (res.affected === 0)
+        return new BadRequestException(`Product with id ${id} does not exits`);
+      return 'ok';
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException(
+        'Unexpected error, check server logs',
+      );
+    }
+  }
+
+  private handleDBExeption(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
   }
 }
